dedf471b5f48b925b138012dd42005d5a029a62e
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-04-03 16:07:15 +0900

    프래그먼트 복귀 => 입력한 닉네임 받아서 반영

--- app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210403/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.pizzaorderapp_20210403.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -29,6 +30,23 @@ class MyProfileFragment : Fragment() {
 
             val myIntent = Intent(activity, EditNicknameActivity::class.java)
             startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_FOR_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val newNickname = data?.getStringExtra("nick")
+
+                nicknameTxt.text = newNickname
+
+            }
 
         }
 
